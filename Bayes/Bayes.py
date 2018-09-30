bded892700a51079a6b61aae02d4e1d406f4b13c
CjhGm <cjh327@126.com>
2018-09-30 22:11:33 +0800

    start bayes

---
+++ Bayes/Bayes.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 21:52:45 2018
+
+@author: tf
+"""
+
---
+++ Bayes/Bayes_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 21:54:05 2018
+
+@author: tf
+"""
+
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', 'problem', 'help', 'please'], 
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+print(loadDataSet())
