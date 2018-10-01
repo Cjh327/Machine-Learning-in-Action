05f7ab7804cd8f855b78ac9e845b90e15bd64506
CjhGm <cjh327@126.com>
2018-10-01 15:34:58 +0800

    4.5.2 completed

--- Bayes/Bayes.py
+++ Bayes/Bayes.py
@@ -5,3 +5,25 @@ Created on Sun Sep 30 21:52:45 2018
 @author: tf
 """
 
+from numpy import *
+
+def trainNB0(trainMatrix: ndarray, trainCategory: ndarray) -> [ndarray, ndarray, int]:
+    '''
+    a naive Bayes
+    trainMatrix:    m*n
+    trainCategory:  m*1
+    '''
+    m = trainMatrix.shape[0]
+    n = trainMatrix.shape[1]
+    pAbusive = trainCategory.sum() / m   # the prob of abusive docement (abusive document: class = 1)
+    
+    positionAbusive = argwhere(trainCategory.reshape(trainCategory.size) == 1)
+    abusiveMat = trainMatrix[positionAbusive.reshape(positionAbusive.size)]
+    p1Num = abusiveMat.sum(axis=0)
+    p0Num = trainMatrix.sum(axis=0) - p1Num
+    p1Denom = abusiveMat.sum()
+    p0Denom = trainMatrix.sum() - p1Denom.sum()
+    p1Vec = p1Num / p1Denom
+    p0Vec = p0Num / p0Denom
+    return p1Vec, p0Vec, pAbusive
+
--- Bayes/Bayes_test.py
+++ Bayes/Bayes_test.py
@@ -5,14 +5,51 @@ Created on Sun Sep 30 21:54:05 2018
 @author: tf
 """
 
-def loadDataSet():
-    postingList = [['my', 'dog', 'has', 'flea', 'problem', 'help', 'please'], 
+import Bayes
+import numpy as np
+
+def loadDataSet() -> [list, list]:
+    postingList =  [['my', 'dog', 'has', 'flea', 'problem', 'help', 'please'], 
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
                    ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
                    ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
                    ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
                    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-    classVec = [0, 1, 0, 1, 0, 1]
+    classVec = [[0], [1], [0], [1], [0], [1]]
     return postingList, classVec
 
-print(loadDataSet())
+
+def createVocabList(dataSet: list) -> list:
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList: list, inputSet: list) -> np.ndarray:
+    retVec = np.zeros((1, len(vocabList)))
+    for word in inputSet:
+        if word in vocabList:
+            retVec[0, vocabList.index(word)] = 1
+        else:
+            print('the word: %s is not in my Vocabulary!' % word)
+    return retVec
+
+def words2Mat(vocabList: list, inputSets: list) -> np.ndarray:
+    retMat = np.zeros((len(inputSets), len(vocabList)))
+    i = 0
+    for wordsSet in inputSets:
+        retMat[i] = setOfWords2Vec(vocabList, wordsSet)
+        i += 1
+    return retMat
+
+listOPosts, listClasses = loadDataSet()
+vocabList = createVocabList(listOPosts)
+print(vocabList)
+vec = setOfWords2Vec(vocabList, listOPosts[0])
+print(vec)
+trainMat = words2Mat(vocabList, listOPosts)
+print(trainMat.shape)
+p1V, p0V, pAb = Bayes.trainNB0(trainMat, np.array(listClasses))
+print('p1V', p1V)
+print('p0V', p0V)
+print('pAb', pAb)
